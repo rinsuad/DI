0c79bdd7af3c3c9b3646bf526d78cd58c78c28d7
Rebeca Insua <rinsuad24@fpcoruna.afundacion.org>
2024-10-01 14:30:04 +0200

    Ejercicio 1: operaciones básicas con python

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,16 @@
+def suma(a,b):
+    print("La suma de ambos valores es: ", a+b)
+
+def resta(a,b):
+    print("La resta del primer valor menos el segundo valor es: ", a-b)
+
+def multiplication(a,b):
+    print("La multiplicación es: ", a*b)
+
+def division(a,b):
+    print("La división es: ", a/b)
+
+suma(2,3)
+resta(2,3)
+multiplication(2,5)
+division(6,2)
