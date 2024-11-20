e201b32abd6d57362aeb62e21ac18a40232982f4
Rebeca Insua <rinsuad24@fpcoruna.afundacion.org>
2024-11-20 11:59:46 +0100

    Ejercicio 3

--- sprint4android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint4android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -1,12 +1,12 @@
 package com.example.mycatalog;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -15,10 +15,10 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        ImageView imageView = findViewById(R.id.avatar);
+
+        Picasso.get().load(R.drawable.image)
+                .transform(new CircleTransform())
+                .into(imageView);
     }
 }
