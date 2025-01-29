c93e743423993ccc8aaf5f8c68423a338f9208e6
Rebeca Insua <rinsuad24@fpcoruna.afundacion.org>
2025-01-29 09:42:11 +0100

    Semana3: Toolbar con título para la Dashboard Activity además de menú con botones

--- MyRecipes/app/src/main/java/myrecipes/app/views/DashboardActivity.java
+++ MyRecipes/app/src/main/java/myrecipes/app/views/DashboardActivity.java
@@ -2,6 +2,11 @@ package myrecipes.app.views;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
@@ -28,7 +33,7 @@ public class DashboardActivity extends AppCompatActivity implements RecipeAdapte
 
         // RecyclerView setup
         binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
+/*
         // Logout button setup
         binding.logoutButton.setOnClickListener(v -> {
             Intent intent = new Intent(this, LoginActivity.class);
@@ -39,7 +44,7 @@ public class DashboardActivity extends AppCompatActivity implements RecipeAdapte
         binding.favouritesButton.setOnClickListener(v -> {
             Intent intent = new Intent(this, FavouriteActivity.class);
             startActivity(intent);
-        });
+        });*/
 
         // Observe recipe data
         viewModel.getRecipeLiveData().observe(this, recipes -> {
@@ -50,6 +55,9 @@ public class DashboardActivity extends AppCompatActivity implements RecipeAdapte
                 ((RecipeAdapter) binding.recyclerView.getAdapter()).updateRecipes(recipes);
             }
         });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
@@ -57,5 +65,24 @@ public class DashboardActivity extends AppCompatActivity implements RecipeAdapte
         Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra("RECIPE_ID", recipe.getId());
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.dashboard_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_favourites) {
+            Intent intent = new Intent(DashboardActivity.this, FavouriteActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.action_logout) {
+            Intent intent = new Intent(DashboardActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
